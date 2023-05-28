2d369933b60013b85d48bcb4592bf35b95680aae
Karan Jakhar <rockingkjakhar@gmail.com>
2023-05-28 18:57:17 +0530

    inital commit - youtube content creator using langchain

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+from apikey import apikey
+
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+
+os.environ['OPENAI_API_KEY'] = apikey
+
+st.title("🦜🔗 Expermiment")
+prompt = st.text_input("Enter your prompt:")
+
+#prompt template
+title_template = PromptTemplate(
+    input_variables=['topic'],
+    template="Write me a youtube video title about {topic}."
+)
+
+script_template = PromptTemplate(
+    input_variables=['title'],
+    template="Write me a youtube video script based on title TITLE: {title}."
+)
+
+#memory
+memory = ConversationBufferMemory(input_key='topic',
+                                  memory_key='chat_memory')
+
+
+#llms
+llm = OpenAI(model='text-babbage-001', temperature=0.9)
+title_chain = LLMChain(llm=llm,
+                       prompt=title_template,
+                       output_key='title',
+                       verbose=True,
+                       memory=memory)
+script_chain = LLMChain(llm=llm,
+                        prompt=script_template,
+                        output_key='script',
+                        verbose=True,
+                        memory=memory)
+sequential_chain = SequentialChain(chains=[title_chain, script_chain],
+                                   input_variables=['topic'],
+                                   output_variables=['title', 'script'],
+                                   verbose=True)
+
+#if there is a prompt, run call llm
+if prompt:
+    response = sequential_chain({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['script'])
+
+    with st.expander("Memory History"):
+        st.write(memory.buffer)
